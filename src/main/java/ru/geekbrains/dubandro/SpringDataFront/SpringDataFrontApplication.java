fd0c4f3f70f424201a4ba0117982a433b2610af8
dubandro <dubovik@internet.ru>
2022-12-12 13:07:27 +0200

    init

---
+++ src/main/java/ru/geekbrains/dubandro/SpringDataFront/SpringDataFrontApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.dubandro.SpringDataFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/dubandro/SpringDataFront/SpringDataFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.dubandro.SpringDataFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
